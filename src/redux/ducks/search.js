06ceef5b137abd5d9d1f86d8260f106fcf5d56bc
Tudor Gergely <tudorgergely@gmail.com>
2017-05-21 16:29:49 +0300

    add duck for search

--- src/redux/ducks/search.js
+++ src/redux/ducks/search.js
@@ -2,12 +2,16 @@ const PREFIX = '@synco/timeline';
 
 const SEARCH = `${PREFIX}/SEARCH`;
 
+const OPEN = `${PREFIX}/OPEN`;
+const CLOSE = `${PREFIX}/CLOSE`;
+
 const LOAD_RECENT_START = `${PREFIX}/LOAD_RECENT_START`;
 const LOAD_RECENT_FINISH = `${PREFIX}/LOAD_RECENT_FINISH`;
 
 const defaultState = {
     term: null,
-    recentSearches: []
+    recentSearches: [],
+    open: false
 };
 
 export default function reducer(state = defaultState, action) {
@@ -21,6 +25,16 @@ export default function reducer(state = defaultState, action) {
             return {
                 ...state,
                 recentSearches: action.recentSearches
+            };
+        case OPEN:
+            return {
+                ...state,
+                open: true
+            };
+        case CLOSE:
+            return {
+                ...state,
+                open: false
             };
         case LOAD_RECENT_FINISH:
         default:
